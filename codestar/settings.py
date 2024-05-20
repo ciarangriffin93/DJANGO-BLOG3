402e2047e0933f290521b5c82e3b2f24eb60173e
Ciaran Griffin <griffinciaran3@gmail.com>
2024-05-20 20:21:55 +0000

    Add Inside migrations

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-ciarangriff-djangoblog3-852pnmkqjur.ws-eu114.gitpod.io', '.herokuapp.com']
 
