c0a70aae6cc3f9ba77eb30b018619ef5325eb802
Ciaran Griffin <griffinciaran3@gmail.com>
2024-05-23 10:20:59 +0000

    generic post view

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-ciarangriff-djangoblog3-pfmu91bry7h.ws-eu114.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriff-djangoblog3-im1h5b46cgt.ws-eu114.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
