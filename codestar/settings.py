07fcf8e023bcef33c8f2b5432d3b7f500ac37031
Ciaran Griffin <griffinciaran3@gmail.com>
2024-05-27 09:27:16 +0000

    deploying

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-ciarangriff-djangoblog3-1572dh6o1d0.ws-eu114.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriff-djangoblog3-6ugektiogl4.ws-eu114.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
