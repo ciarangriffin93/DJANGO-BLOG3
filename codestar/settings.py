c79104125e27a1031fbe3095e0d7f50384eea143
Ciaran Griffin <griffinciaran3@gmail.com>
2024-05-30 21:33:44 +0000

    add post_deataul.html

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-ciarangriff-djangoblog3-8u58ifvu6i9.ws.codeinstitute-ide.net', '.herokuapp.com']
 
