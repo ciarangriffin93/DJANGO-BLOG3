c7ab02acb43101bd844ed2d5af0643088ed88070
Ciaran Griffin <griffinciaran3@gmail.com>
2024-05-30 19:49:53 +0000

    Adding more posts

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ciarangriff-djangoblog3-8kw9gufy3a5.ws.codeinstitute-ide.net', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriff-djangoblog3-m2puqkmawkn.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
@@ -93,8 +93,7 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://*.gitpod.io",
-    "https://*.herokuapp.com"
+    'https://8000-ciarangriff-djangoblog3-m2puqkmawkn.ws.codeinstitute-ide.net'
 ]
 
 
