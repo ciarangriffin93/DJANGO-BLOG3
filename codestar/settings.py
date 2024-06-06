3fa4ab737b8a1eceb2dfa1769a97ef331746aa92
Ciaran Griffin <griffinciaran3@gmail.com>
2024-06-06 20:07:47 +0000

    Fixed all work

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,9 +28,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-ciarangriff-djangoblog3-8u58ifvu6i9.ws.codeinstitute-ide.net', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriff-djangoblog3-8i067apatet.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
@@ -93,7 +93,7 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-ciarangriff-djangoblog3-8u58ifvu6i9.ws.codeinstitute-ide.net'
+    'https://8000-ciarangriff-djangoblog3-8i067apatet.ws.codeinstitute-ide.net'
 ]
 
 
